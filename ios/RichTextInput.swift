2ba57914285b71176138ecfc613584490ba28bb0
Islam Rustamov <islamrustamov@rambler.ru>
2024-10-01 22:19:03 +0300

    chore(rich-text): add indentation

--- ios/RichTextInput.swift
+++ ios/RichTextInput.swift
@@ -11,38 +11,38 @@ import Foundation
 // Define all props here
 // UPDATE: okay, now this class is also needed for helping functions
 class RichTextInput: UITextView, UIEditMenuInteractionDelegate {
-  @objc var placeholder: String = "Placeholder"
-
-  @objc var onChange: RCTBubblingEventBlock? = nil
-  
-  override func editMenu(for textRange: UITextRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
-    var option: UIMenu.Options? = nil
+    @objc var placeholder: String = "Placeholder"
     
-    if #available(iOS 15.0, *) {
-      option = .singleSelection
-    } else {
-      option = .displayInline
+    @objc var onChange: RCTBubblingEventBlock? = nil
+    
+    override func editMenu(for textRange: UITextRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
+        var option: UIMenu.Options? = nil
+        
+        if #available(iOS 15.0, *) {
+            option = .singleSelection
+        } else {
+            option = .displayInline
+        }
+        
+        let customMenu = UIMenu(title: "Format", options: option ?? .displayInline, children: [
+            UIAction(title: "Bold") { _ in
+                self.toggleFont(trait: .traitBold)
+            },
+            UIAction(title: "Italic") { _ in
+                self.toggleFont(trait: .traitItalic)
+            },
+            UIAction(title: "Strikethrough") { _ in
+                self.toggleStyle(style: NSAttributedString.Key.strikethroughStyle)
+            },
+            UIAction(title: "Underline") { _ in
+                self.toggleStyle(style: NSAttributedString.Key.underlineStyle)
+            }
+        ])
+        
+        var actions = suggestedActions
+        actions.insert(customMenu, at: 0)
+        return UIMenu(children: actions)
     }
-
-    let customMenu = UIMenu(title: "Format", options: option ?? .displayInline, children: [
-      UIAction(title: "Bold") { _ in
-        self.toggleFont(trait: .traitBold)
-      },
-      UIAction(title: "Italic") { _ in
-        self.toggleFont(trait: .traitItalic)
-      },
-      UIAction(title: "Strikethrough") { _ in
-        self.toggleStyle(style: NSAttributedString.Key.strikethroughStyle)
-      },
-      UIAction(title: "Underline") { _ in
-        self.toggleStyle(style: NSAttributedString.Key.underlineStyle)
-      }
-    ])
-
-    var actions = suggestedActions
-    actions.insert(customMenu, at: 0)
-    return UIMenu(children: actions)
-  }
     
     func focus() {
         reactFocus()
@@ -51,102 +51,102 @@ class RichTextInput: UITextView, UIEditMenuInteractionDelegate {
     func blur() {
         reactBlur()
     }
-  
-  // TODO: styles are getting lost if you enable/reenable them, bug, needs fixing
-  func toggleStyle(style: NSAttributedString.Key) {
-    if selectedRange.length == 0 {
-      return
-    }
     
-    let attribute = textStorage.attribute(
-      style,
-      at: selectedRange.location,
-      longestEffectiveRange: nil,
-      in: selectedRange
-    )
-    
-    let isStyleEnabled = attribute != nil
-    
-    if (isStyleEnabled) {
-      textStorage.removeAttribute(
-        style,
-        range: selectedRange
-      )
-    } else {
-      textStorage.addAttribute(style, value: NSUnderlineStyle.single.rawValue, range: selectedRange)
-    }
-  }
-  
-  func toggleFont(trait: UIFontDescriptor.SymbolicTraits.Element) {
-    if selectedRange.length == 0 {
-      return
-    }
-
-    let fontAttribute = textStorage.attribute(
-      NSAttributedString.Key.font,
-      at: selectedRange.location,
-      longestEffectiveRange: nil,
-      in: selectedRange
-    )
-    
-    guard let font = fontAttribute as? UIFont else {
-      textStorage.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 16, symbolicTraits: trait), range: selectedRange)
-      return
-    }
+    // TODO: styles are getting lost if you enable/reenable them, bug, needs fixing
+    func toggleStyle(style: NSAttributedString.Key) {
+        if selectedRange.length == 0 {
+            return
+        }
         
-    let traits = font.fontDescriptor.symbolicTraits
-    let isFontAdded = traits.contains(trait)
+        let attribute = textStorage.attribute(
+            style,
+            at: selectedRange.location,
+            longestEffectiveRange: nil,
+            in: selectedRange
+        )
         
-    textStorage.enumerateAttribute(.font, in: selectedRange, options: .longestEffectiveRangeNotRequired) { (value, range, stop) in
-        if let font = value as? UIFont {
-          textStorage.removeAttribute(.font, range: range)
-          
-          var traits: UIFontDescriptor.SymbolicTraits = UIFontDescriptor.SymbolicTraits()
-          
-          traits.insert(font.fontDescriptor.symbolicTraits)
-          
-          if isFontAdded {
-              traits.remove(trait)
-          } else {
-              traits.insert(trait)
-          }
-          
-          textStorage.addAttribute(
-            .font,
-            value: UIFont(descriptor: font.fontDescriptor.withSymbolicTraits(traits)!, size: font.pointSize),
-            range: range
-          )
+        let isStyleEnabled = attribute != nil
+        
+        if (isStyleEnabled) {
+            textStorage.removeAttribute(
+                style,
+                range: selectedRange
+            )
+        } else {
+            textStorage.addAttribute(style, value: NSUnderlineStyle.single.rawValue, range: selectedRange)
         }
     }
-  }
-
-  @discardableResult func async<T>(_ block: @escaping () -> T) -> T {
-    let queue = DispatchQueue.global()
-    let group = DispatchGroup()
-    var result: T?
-    group.enter()
-    queue.async(group: group) { result = block(); group.leave(); }
-    group.wait()
-
-    return result!
-  }
-  
-  // TODO: does it have to be so complicated?
-  // NOTE: semaphors are needed cause textstorage requires to work on main thread
-  func getHTML() -> String {
-    var value: String = ""
     
-    let semaphore = DispatchSemaphore(value: 0)
-
-    DispatchQueue.main.async {
-      value = self.attributedText.toHTML() ?? ""
-      semaphore.signal()
+    func toggleFont(trait: UIFontDescriptor.SymbolicTraits.Element) {
+        if selectedRange.length == 0 {
+            return
+        }
+        
+        let fontAttribute = textStorage.attribute(
+            NSAttributedString.Key.font,
+            at: selectedRange.location,
+            longestEffectiveRange: nil,
+            in: selectedRange
+        )
+        
+        guard let font = fontAttribute as? UIFont else {
+            textStorage.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 16, symbolicTraits: trait), range: selectedRange)
+            return
+        }
+        
+        let traits = font.fontDescriptor.symbolicTraits
+        let isFontAdded = traits.contains(trait)
+        
+        textStorage.enumerateAttribute(.font, in: selectedRange, options: .longestEffectiveRangeNotRequired) { (value, range, stop) in
+            if let font = value as? UIFont {
+                textStorage.removeAttribute(.font, range: range)
+                
+                var traits: UIFontDescriptor.SymbolicTraits = UIFontDescriptor.SymbolicTraits()
+                
+                traits.insert(font.fontDescriptor.symbolicTraits)
+                
+                if isFontAdded {
+                    traits.remove(trait)
+                } else {
+                    traits.insert(trait)
+                }
+                
+                textStorage.addAttribute(
+                    .font,
+                    value: UIFont(descriptor: font.fontDescriptor.withSymbolicTraits(traits)!, size: font.pointSize),
+                    range: range
+                )
+            }
+        }
     }
     
-    semaphore.wait()
+    @discardableResult func async<T>(_ block: @escaping () -> T) -> T {
+        let queue = DispatchQueue.global()
+        let group = DispatchGroup()
+        var result: T?
+        group.enter()
+        queue.async(group: group) { result = block(); group.leave(); }
+        group.wait()
+        
+        return result!
+    }
     
-    return value
-  }
+    // TODO: does it have to be so complicated?
+    // NOTE: semaphors are needed cause textstorage requires to work on main thread
+    func getHTML() -> String {
+        var value: String = ""
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        DispatchQueue.main.async {
+            value = self.attributedText.toHTML() ?? ""
+            semaphore.signal()
+        }
+        
+        semaphore.wait()
+        
+        return value
+    }
 }
 
 extension NSAttributedString {
--- ios/RichTextInputViewManager.swift
+++ ios/RichTextInputViewManager.swift
@@ -11,136 +11,136 @@ import React
 
 @objc (RichTextInputViewManager)
 public class RichTextInputViewManager: RCTViewManager, UITextViewDelegate  {
-  var richTextView: RichTextInput? = nil
-
-  public override static func requiresMainQueueSetup() -> Bool {
-      return true
-  }
-
-  @objc
-  public override func view() -> UITextView {
-      let view = RichTextInput()
-
-      view.font = .systemFont(ofSize: 16)
-
-      view.text = view.placeholder
-      view.textColor = UIColor.lightGray
+    var richTextView: RichTextInput? = nil
     
-      view.delegate = self
+    public override static func requiresMainQueueSetup() -> Bool {
+        return true
+    }
+    
+    @objc
+    public override func view() -> UITextView {
+        let view = RichTextInput()
         
-      richTextView = view
+        view.font = .systemFont(ofSize: 16)
         
-      return view
-  }
-  
-  @objc
-  func setPlaceholder(_ node: NSNumber, placeholder: String) {
-      // NOTE: this is a bad approach
-      executeBlock({ (richTextView) in
-        self.richTextView?.placeholder = placeholder
-        self.richTextView?.text = placeholder
-      }, onNode: node)
-  }
-  
-  @objc
-  func toggleUnderline(_ node: NSNumber) {
-      executeBlock({ (richTextView) in
-        richTextView.toggleStyle(style: NSAttributedString.Key.underlineStyle)
-      }, onNode: node)
-  }
+        view.text = view.placeholder
+        view.textColor = UIColor.lightGray
+        
+        view.delegate = self
+        
+        richTextView = view
+        
+        return view
+    }
+    
+    @objc
+    func setPlaceholder(_ node: NSNumber, placeholder: String) {
+        // NOTE: this is a bad approach
+        executeBlock({ (richTextView) in
+            self.richTextView?.placeholder = placeholder
+            self.richTextView?.text = placeholder
+        }, onNode: node)
+    }
+    
+    @objc
+    func toggleUnderline(_ node: NSNumber) {
+        executeBlock({ (richTextView) in
+            richTextView.toggleStyle(style: NSAttributedString.Key.underlineStyle)
+        }, onNode: node)
+    }
     
     @objc
     func focus(_ node: NSNumber) {
         executeBlock({ (richTextView) in
-          richTextView.focus()
+            richTextView.focus()
         }, onNode: node)
     }
-
+    
     @objc
     func blur(_ node: NSNumber) {
         executeBlock({ (richTextView) in
-          richTextView.blur()
+            richTextView.blur()
         }, onNode: node)
     }
-  
-  @objc
-  func toggleStrike(_ node: NSNumber) {
-      executeBlock({ (richTextView) in
-        richTextView.toggleStyle(style: NSAttributedString.Key.strikethroughStyle)
-      }, onNode: node)
-  }
-  
-  @objc
-  func toggleBold(_ node: NSNumber) {
-      executeBlock({ (richTextView) in
-        richTextView.toggleFont(trait: .traitBold)
-      }, onNode: node)
-  }
-  
-  @objc
-  func toggleItalic(_ node: NSNumber) {
-      executeBlock({ (richTextView) in
-        richTextView.toggleFont(trait: .traitItalic)
-      }, onNode: node)
-  }
-
-  @objc
-  func getHTML() -> String {
-    return richTextView?.getHTML() ?? "ERROR: richTextView IS NOT INITIALIZED"
-  }
-  
-  // Helpers
-  func executeBlock(_ block: @escaping (RichTextInput) -> Void, onNode node: NSNumber) {
-      self.bridge.uiManager.addUIBlock { (manager, viewRegistry) in
-        guard let richText = self.richTextView else {
-            return
-        }
-        block(richText)
-      }
-  }
     
-  public func textViewDidBeginEditing(_ textView: UITextView) {
-      if textView.textColor == UIColor.lightGray {
-          textView.text = nil
-          textView.textColor = UIColor.black
-      }
-  }
-  
-  public func textViewDidEndEditing(_ textView: UITextView) {
-      if textView.text.isEmpty {
-          textView.text = richTextView?.placeholder ?? "Placeholder"
-          textView.textColor = UIColor.lightGray
-      }
-  }
-
-  // NOTE: huge type mismatching of UITextView and RichTextInput
-  // is there a proper way to do that?
-  public func textViewDidChange(_ textView: UITextView) {
-      if let onChange = richTextView?.onChange {
-          onChange(["text": textView.text ?? ""])
-      }
-  }
+    @objc
+    func toggleStrike(_ node: NSNumber) {
+        executeBlock({ (richTextView) in
+            richTextView.toggleStyle(style: NSAttributedString.Key.strikethroughStyle)
+        }, onNode: node)
+    }
+    
+    @objc
+    func toggleBold(_ node: NSNumber) {
+        executeBlock({ (richTextView) in
+            richTextView.toggleFont(trait: .traitBold)
+        }, onNode: node)
+    }
+    
+    @objc
+    func toggleItalic(_ node: NSNumber) {
+        executeBlock({ (richTextView) in
+            richTextView.toggleFont(trait: .traitItalic)
+        }, onNode: node)
+    }
+    
+    @objc
+    func getHTML() -> String {
+        return richTextView?.getHTML() ?? "ERROR: richTextView IS NOT INITIALIZED"
+    }
+    
+    // Helpers
+    func executeBlock(_ block: @escaping (RichTextInput) -> Void, onNode node: NSNumber) {
+        self.bridge.uiManager.addUIBlock { (manager, viewRegistry) in
+            guard let richText = self.richTextView else {
+                return
+            }
+            block(richText)
+        }
+    }
+    
+    public func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    public func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = richTextView?.placeholder ?? "Placeholder"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    // NOTE: huge type mismatching of UITextView and RichTextInput
+    // is there a proper way to do that?
+    public func textViewDidChange(_ textView: UITextView) {
+        if let onChange = richTextView?.onChange {
+            onChange(["text": textView.text ?? ""])
+        }
+    }
 }
 
 extension UIFont {
-  class func systemFont(ofSize fontSize: CGFloat, symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
-      return UIFont.systemFont(ofSize: fontSize).including(symbolicTraits: symbolicTraits)
-  }
-
-  func including(symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
-      var _symbolicTraits = self.fontDescriptor.symbolicTraits
-      _symbolicTraits.update(with: symbolicTraits)
-      return withOnly(symbolicTraits: _symbolicTraits)
-  }
-
-  func excluding(symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
-      var _symbolicTraits = self.fontDescriptor.symbolicTraits
-      _symbolicTraits.remove(symbolicTraits)
-      return withOnly(symbolicTraits: _symbolicTraits)
-  }
-
-  func withOnly(symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
-      guard let fontDescriptor = fontDescriptor.withSymbolicTraits(symbolicTraits) else { return nil }
-      return .init(descriptor: fontDescriptor, size: pointSize)
-  }
+    class func systemFont(ofSize fontSize: CGFloat, symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
+        return UIFont.systemFont(ofSize: fontSize).including(symbolicTraits: symbolicTraits)
+    }
+    
+    func including(symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
+        var _symbolicTraits = self.fontDescriptor.symbolicTraits
+        _symbolicTraits.update(with: symbolicTraits)
+        return withOnly(symbolicTraits: _symbolicTraits)
+    }
+    
+    func excluding(symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
+        var _symbolicTraits = self.fontDescriptor.symbolicTraits
+        _symbolicTraits.remove(symbolicTraits)
+        return withOnly(symbolicTraits: _symbolicTraits)
+    }
+    
+    func withOnly(symbolicTraits: UIFontDescriptor.SymbolicTraits) -> UIFont? {
+        guard let fontDescriptor = fontDescriptor.withSymbolicTraits(symbolicTraits) else { return nil }
+        return .init(descriptor: fontDescriptor, size: pointSize)
+    }
 }
